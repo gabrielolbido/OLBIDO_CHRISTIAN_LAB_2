6c42c102256d53dbf68a86288df50e85892a20db
Gabriel Olbido <2015081593@ust-ics.mygbiz.com>
2018-09-10 21:57:26 +0800

    :)

---
+++ app/src/main/java/com/olbido/christian/relative.java
@@ -0,0 +1,13 @@
+package com.olbido.christian;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class relative extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative);
+    }
+}
